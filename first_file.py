fc7b252afa786bfddd71e94ec000c98617ad19d7
santhosh121 <santhosh.swargam87@gmail.com>
2025-05-23 15:45:57 -0400

    completed

--- first_file.py
+++ first_file.py
@@ -1,8 +1,3 @@
 print ('hello world')
 print('secong hello')
-<<<<<<< HEAD
-print('forth hello')
-print ('checking git fetch")
-=======
 print('checking git pull')
->>>>>>> 6c45feb (Update first_file.py)
